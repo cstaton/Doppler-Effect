983e24fd68eff308f16535adb161125d882ee70e
Chris <cpa.staton@gmail.com>
2015-08-14 18:28:19 -0700

    Update server to push to heroku

--- myServer.js
+++ myServer.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.set("port", (process.env.PORT || 3030));
+
 app.use(express.static(__dirname + "/client"));
 
-app.listen(3030);
-console.log("Listening on port 3030");
+app.listen(app.get("port"));
+
+console.log("Listening on port");
